0eeee3472c1ae702b4b62be897ac23c3743590bc
starodubdmitriy <starodubdmitriy@gmail.com>
2018-05-11 23:03:53 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('registration', 'API\RegistrationsController@registration');
+Route::post('registration', 'API\RegistrationsController@registration');
 
 // routes for login users
 Route::group(['middleware' => ['api_auth']], function () {
 
     Route::get('user/details', 'API\UsersController@details');
 
-    Route::get('note/create', 'API\NotesController@create');
+    Route::post('note/create', 'API\NotesController@create');
 
-    Route::get('note/update', 'API\NotesController@update');
+    Route::post('note/update', 'API\NotesController@update');
 
     Route::get('note', 'API\NotesController@index');
 
